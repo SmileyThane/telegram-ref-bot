9cf96b1eec97167dff2939aea184ce1ce9c6224c
Hleb Prakhnitski <thane.crios@gmail.com>
2023-03-02 22:31:16 +0400

    implemented new fields

---
+++ database/migrations/2023_03_01_182846_add_fields_at_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFieldsAtUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username')->nullable();
+            $table->text('card_details')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('username');
+            $table->dropColumn('card_details');
+        });
+    }
+}
