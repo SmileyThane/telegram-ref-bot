e92b7651f5058c459f1377bfc5b7fa14df30bb3e
Hleb Prakhnitski <thane.crios@gmail.com>
2023-04-16 20:47:20 +0400

    minor fix

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,6 +17,7 @@ class UserSeeder extends Seeder
         User::query()->where('id', '=', 1)->delete();
         $user = new User();
         $user->id = 1;
+        $user->name = 'admin';
         $user->email = 'admin@tg.test';
         $user->password = bcrypt('TG12345678');
         $user->telegram_id = '__admin__';
