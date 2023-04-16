677244d4235c8ff6116de96f50e245e6e6cf9946
Hleb Prakhnitski <thane.crios@gmail.com>
2023-04-16 20:16:36 +0400

    created default user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::query()->where('id', '=', 1)->delete();
+        $user = new User();
+        $user->id = 1;
+        $user->email = 'admin@tg.test';
+        $user->password = bcrypt('TG12345678');
+        $user->telegram_id = '__admin__';
+        $user->save();
+    }
+}
